6d09d218df007f8237a6ed6aec2c005797e70fbf
FeritYILDIZ <feritstar@gmail.com>
2024-04-13 17:17:42 +0300

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+#include "Main.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// ifstream:
+	// ifstream is input file stream which allows you to read the contents of a file.
+
+	// ofstream
+	// ofstream is output file stream which allows you to write contents to a file.
+
+	// fstream:
+	// fstream allows both reading fromand writing to files by default.
+	// however, you can have an fstream behave like an ifstream or ofstream
+	// by passing in the ios::open_mode flag.
+
+
+
+
+	cout << endl;
+	return 0;
+}
